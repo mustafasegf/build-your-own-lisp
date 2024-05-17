1e6899e3f41039c364f49c5f701fb85bb35cdf66
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-05-17 23:35:18 +0700

    chore: use nix because i hate my life

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+          packages = with pkgs;
+            [ clang-tools cmake editline ninja ]
+            ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+
+          CFLAGS = "-I${pkgs.editline.dev}/include";
+        };
+
+      });
+    };
+}
