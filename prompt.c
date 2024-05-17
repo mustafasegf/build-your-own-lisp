63dbea3ae0554b5d611f352505f087bc535f9fa1
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-05-17 23:35:39 +0700

    feat: chapter 4 add windows cuz why not

--- prompt.c
+++ prompt.c
@@ -1,15 +1,39 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char *readline(char *prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char *cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char *unused) {}
+
+#else
+#include <editline.h>
+#endif
 
 int main() {
   puts("Lispy Version 0.1");
   puts("Press Ctrl+c to Exit\n");
 
   while (true) {
-    fputs("lispy> ", stdout);
-    fgets(input, 2048, stdin);
-    printf("No you're a %s", input);
+    char *input = readline("lispy> ");
+    add_history(input);
+
+    printf("No you're a %s\n", input);
+    free(input);
   }
+  return 0;
 }
